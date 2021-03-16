567b8d51b5bff6b902a081dd6785681260728cdf
testmapper <testmapper@yandex.ru>
2021-03-16 16:20:47 +0300

    123

---
+++ Получение всех работ.py
@@ -0,0 +1,17 @@
+import flask
+
+from data import db_session
+from data.jobs import Jobs
+
+blueprint = flask.Blueprint('jobs_api', __name__, template_folder='templates')
+
+
+@blueprint.route('/api/jobs')
+def get_jobs():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return flask.jsonify({'jobs': [item.to_dict(
+        only=('id', 'job', 'team_leader', 'work_size', 'collaborators',
+              'start_date', 'end_date', 'is_finished', 'category')
+    )
+        for item in jobs]})
